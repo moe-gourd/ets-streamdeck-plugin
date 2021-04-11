4e5c9c0ddd07d6c3a6b530794fbd72bf724d7476
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-11 09:14:27 +0200

    initial project setup

---
+++ src/ETS2Plugin/ETS2Plugin.cpp
@@ -0,0 +1,9 @@
+//==============================================================================
+
+
+#include <iostream>
+
+int main(int argc, const char* const argv[])
+{
+    std::cout << "Hello World!\n";
+}
