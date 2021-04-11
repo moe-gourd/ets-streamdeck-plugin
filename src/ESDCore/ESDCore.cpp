9323d9881a2542bed78e963f3d637db8ca0a744f
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-11 10:56:55 +0200

    Elgato Stream Deck Core Library Projects added

---
+++ src/ESDCore/ESDCore.cpp
@@ -0,0 +1,9 @@
+// ESDCore.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnESDCore()
+{
+}
---
+++ src/ESDCoreTest/ESDCoreTest.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ESDCoreTest
+{
+	TEST_CLASS(ESDCoreTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
