d60964d3162b5c64fd5a5571e4dcf505fb94a481
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-17 21:30:57 +0200

    copy from connectiion manager

--- src/ESDCore/WebsocketConnection.cpp
+++ src/ESDCore/WebsocketConnection.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "WebsocketConnection.h"
+#include "JSONUtils.h"
 
 WebsocketConnection::WebsocketConnection(std::string registerEvent, std::string pluginUUID)
 {
@@ -7,14 +8,46 @@ WebsocketConnection::WebsocketConnection(std::string registerEvent, std::string 
 
 void WebsocketConnection::OnOpen(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
 {
+	DebugPrint("OnOpen");
+
+	json jsonObject;
+	jsonObject["event"] = registerEvent;
+	jsonObject["uuid"] = pluginUUID;
+
+	websocketpp::lib::error_code errorCode;
+	webSocketClient.send(connectionHandle, jsonObject.dump(), websocketpp::frame::opcode::text, errorCode);
 }
 
 void WebsocketConnection::OnFail(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
 {
+	std::string reason;
+
+	if (nullptr == inClient)
+	{
+		WebsocketClient::connection_ptr connection = inClient->get_con_from_hdl(inConnectionHandler);
+		if (connection != NULL)
+		{
+			reason = connection->get_ec().message();
+		}
+	}
+
+	DebugPrint("Failed with reason: %s\n", reason.c_str());
 }
 
 void WebsocketConnection::OnClose(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
 {
+	std::string reason;
+
+	if (inClient != nullptr)
+	{
+		WebsocketClient::connection_ptr connection = inClient->get_con_from_hdl(inConnectionHandler);
+		if (connection != NULL)
+		{
+			reason = connection->get_remote_close_reason();
+		}
+	}
+
+	DebugPrint("Close with reason: %s\n", reason.c_str());
 }
 
 void WebsocketConnection::OnMessage(websocketpp::connection_hdl, WebsocketClient::message_ptr inMsg)
@@ -23,4 +56,44 @@ void WebsocketConnection::OnMessage(websocketpp::connection_hdl, WebsocketClient
 
 void WebsocketConnection::connect(int port)
 {
+	try
+	{
+		// Create the endpoint
+		webSocketClient.clear_access_channels(websocketpp::log::alevel::all);
+		webSocketClient.clear_error_channels(websocketpp::log::elevel::all);
+
+		// Initialize ASIO
+		webSocketClient.init_asio();
+
+		// Register our message handler
+		webSocketClient.set_open_handler(websocketpp::lib::bind(&WebsocketConnection::OnOpen, this, &webSocketClient, websocketpp::lib::placeholders::_1));
+		webSocketClient.set_fail_handler(websocketpp::lib::bind(&WebsocketConnection::OnFail, this, &webSocketClient, websocketpp::lib::placeholders::_1));
+		webSocketClient.set_close_handler(websocketpp::lib::bind(&WebsocketConnection::OnClose, this, &webSocketClient, websocketpp::lib::placeholders::_1));
+		webSocketClient.set_message_handler(websocketpp::lib::bind(&WebsocketConnection::OnMessage, this, websocketpp::lib::placeholders::_1, websocketpp::lib::placeholders::_2));
+
+		websocketpp::lib::error_code ec;
+		std::string uri = "ws://127.0.0.1:" + std::to_string(port);
+		WebsocketClient::connection_ptr connection = webSocketClient.get_connection(uri, ec);
+		if (ec)
+		{
+			DebugPrint("Connect initialization error: %s\n", ec.message().c_str());
+			return;
+		}
+
+		connectionHandle = connection->get_handle();
+
+		// Note that connect here only requests a connection. No network messages are
+		// exchanged until the event loop starts running in the next line.
+		webSocketClient.connect(connection);
+
+		// Start the ASIO io_service run loop
+		// this will cause a single connection to be made to the server. mWebsocket.run()
+		// will exit when this connection is closed.
+		webSocketClient.run();
+	}
+	catch (websocketpp::exception const& e)
+	{
+		const char* reason = e.what();
+		DebugPrint("Websocket threw an exception: %s\n", reason);
+	}
 }
