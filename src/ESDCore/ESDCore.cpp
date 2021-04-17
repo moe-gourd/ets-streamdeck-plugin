998ebef0980f72c9d6a688593e08994bb25ccdb3
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-17 17:51:36 +0200

    remove deprecated files

--- src/ESDCore/ESDCore.cpp
+++
@@ -1,9 +0,0 @@
-// ESDCore.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
-//
-
-#include "framework.h"
-
-// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
-void fnESDCore()
-{
-}
