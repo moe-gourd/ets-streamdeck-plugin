ea5f109a4ef3b3504ae697332d44aab0e1717131
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-17 21:27:23 +0200

    interface definition

--- src/ESDCore/WebsocketConnection.cpp
+++ src/ESDCore/WebsocketConnection.cpp
@@ -1,2 +1,26 @@
 #include "pch.h"
 #include "WebsocketConnection.h"
+
+WebsocketConnection::WebsocketConnection(std::string registerEvent, std::string pluginUUID)
+{
+}
+
+void WebsocketConnection::OnOpen(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
+{
+}
+
+void WebsocketConnection::OnFail(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
+{
+}
+
+void WebsocketConnection::OnClose(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler)
+{
+}
+
+void WebsocketConnection::OnMessage(websocketpp::connection_hdl, WebsocketClient::message_ptr inMsg)
+{
+}
+
+void WebsocketConnection::connect(int port)
+{
+}
--- src/ESDCore/WebsocketConnection.h
+++ src/ESDCore/WebsocketConnection.h
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+#include <websocketpp/common/thread.hpp>
+#include <websocketpp/common/memory.hpp>
+
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+typedef websocketpp::client<websocketpp::config::asio_client> WebsocketClient;
+
 class WebsocketConnection
 {
+private:
+	WebsocketClient webSocketClient;
+	websocketpp::connection_hdl connectionHandle;
+
+	std::string registerEvent;
+	std::string pluginUUID;
+
+	// Websocket callbacks
+	void OnOpen(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler);
+	void OnFail(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler);
+	void OnClose(WebsocketClient* inClient, websocketpp::connection_hdl inConnectionHandler);
+	void OnMessage(websocketpp::connection_hdl, WebsocketClient::message_ptr inMsg);
+
+public:
+	WebsocketConnection(std::string registerEvent, std::string pluginUUID);
+
+	void connect(int port);
 };
 
