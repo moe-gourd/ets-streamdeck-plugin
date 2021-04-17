9b9fcc26bc11ace63a7c3962f18e64a020fba9bd
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-17 18:07:06 +0200

    added debug function

--- src/ESDCore/pch.h
+++ src/ESDCore/pch.h
@@ -1,6 +1,35 @@
 #pragma once
 
 //-------------------------------------------------------------------
+// C++ headers
+//-------------------------------------------------------------------
+
+#include <winsock2.h>
+#include <Windows.h>
+#include <string>
+#include <set>
+#include <thread>
+#include <strsafe.h>
+
+//-------------------------------------------------------------------
+// Debug logging
+//-------------------------------------------------------------------
+
+#ifdef _DEBUG
+#define DEBUG 1
+#else
+#define DEBUG 0
+#endif
+
+void __cdecl dbgprintf(const char* format, ...);
+
+#if DEBUG
+#define DebugPrint			dbgprintf
+#else
+#define DebugPrint(...)		while(0)
+#endif
+
+//-------------------------------------------------------------------
 // websocketpp
 //-------------------------------------------------------------------
 
