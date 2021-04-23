5b881b35aa0567f1f20cb4a025c3bfdb3f4fff43
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-23 09:39:57 +0200

    created subclasses for all Received Events

--- src/ESDCore/ReceivedEvent.cpp
+++ src/ESDCore/ReceivedEvent.cpp
@@ -22,3 +22,68 @@ ReceivedEvent::ReceivedEvent(std::string event, json jsonMessage): event(event)
 	json payload;
 	JSONUtils::getObjectByName(jsonMessage, "payload", payload);
 }
+
+DidReceiveSettingsEvent::DidReceiveSettingsEvent(std::string event, json jsonMessage): ReceivedEvent(event, jsonMessage)
+{
+}
+
+DidReceiveGlobalSettingsEvent::DidReceiveGlobalSettingsEvent(std::string event, json jsonMessage): ReceivedEvent(event, jsonMessage)
+{
+}
+
+KeyDownEvent::KeyDownEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+
+KeyUpEvent::KeyUpEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+WillAppearEvent::WillAppearEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+WillDisappearEvent::WillDisappearEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+TitleParametersDidChangeEvent::TitleParametersDidChangeEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+DeviceDidConnectEvent::DeviceDidConnectEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+DeviceDidDisconnectEvent::DeviceDidDisconnectEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+ApplicationDidLaunchEvent::ApplicationDidLaunchEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+ApplicationDidTerminateEvent::ApplicationDidTerminateEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+SystemDidWakeUpEvent::SystemDidWakeUpEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+PropertyInspectorDidAppearEvent::PropertyInspectorDidAppearEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+PropertyInspectorDidDisappearEvent::PropertyInspectorDidDisappearEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+SendToPluginEvent::SendToPluginEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
+
+SendToPropertyInspectorEvent::SendToPropertyInspectorEvent(std::string event, json jsonMessage) : ReceivedEvent(event, jsonMessage)
+{
+}
--- src/ESDCore/ReceivedEvent.h
+++ src/ESDCore/ReceivedEvent.h
@@ -14,3 +14,99 @@ private:
 protected:
 	ReceivedEvent(std::string event, json jsonMessage);
 };
+
+class DidReceiveSettingsEvent : public ReceivedEvent
+{
+public:
+	DidReceiveSettingsEvent(std::string event, json jsonMessage);
+};
+
+class DidReceiveGlobalSettingsEvent : public ReceivedEvent
+{
+public:
+	DidReceiveGlobalSettingsEvent(std::string event, json jsonMessage);
+};
+
+class KeyDownEvent : public ReceivedEvent
+{
+public:
+	KeyDownEvent(std::string event, json jsonMessage);
+};
+
+class KeyUpEvent : public ReceivedEvent
+{
+public:
+	KeyUpEvent(std::string event, json jsonMessage);
+};
+
+class WillAppearEvent : public ReceivedEvent
+{
+public:
+	WillAppearEvent(std::string event, json jsonMessage);
+};
+
+class WillDisappearEvent : public ReceivedEvent
+{
+public:
+	WillDisappearEvent(std::string event, json jsonMessage);
+};
+
+class TitleParametersDidChangeEvent : public ReceivedEvent
+{
+public:
+	TitleParametersDidChangeEvent(std::string event, json jsonMessage);
+};
+
+class DeviceDidConnectEvent : public ReceivedEvent
+{
+public:
+	DeviceDidConnectEvent(std::string event, json jsonMessage);
+};
+
+class DeviceDidDisconnectEvent : public ReceivedEvent
+{
+public:
+	DeviceDidDisconnectEvent(std::string event, json jsonMessage);
+};
+
+class ApplicationDidLaunchEvent : public ReceivedEvent
+{
+public:
+	ApplicationDidLaunchEvent(std::string event, json jsonMessage);
+};
+
+class ApplicationDidTerminateEvent : public ReceivedEvent
+{
+public:
+	ApplicationDidTerminateEvent(std::string event, json jsonMessage);
+};
+
+class SystemDidWakeUpEvent : public ReceivedEvent
+{
+public:
+	SystemDidWakeUpEvent(std::string event, json jsonMessage);
+};
+
+class PropertyInspectorDidAppearEvent : public ReceivedEvent
+{
+public:
+	PropertyInspectorDidAppearEvent(std::string event, json jsonMessage);
+};
+
+class PropertyInspectorDidDisappearEvent : public ReceivedEvent
+{
+public:
+	PropertyInspectorDidDisappearEvent(std::string event, json jsonMessage);
+};
+
+class SendToPluginEvent : public ReceivedEvent
+{
+public:
+	SendToPluginEvent(std::string event, json jsonMessage);
+};
+
+class SendToPropertyInspectorEvent : public ReceivedEvent
+{
+public:
+	SendToPropertyInspectorEvent(std::string event, json jsonMessage);
+};
