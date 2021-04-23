21365a8e17a0c6ae2453b6675c3d7b0ce8655139
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-23 13:46:23 +0200

    initial revision plugin base class

---
+++ src/ESDCore/Plugin.cpp
@@ -0,0 +1,70 @@
+#include "pch.h"
+#include "Plugin.h"
+
+Plugin::Plugin()
+{
+}
+
+void Plugin::didReceiveSettings(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::didReceiveGlobalSettings(json& payload)
+{
+}
+
+void Plugin::keyDown(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::keyUp(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::willAppear(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::willDisappear(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::titleParametersDidChange(std::string& action, std::string& context, std::string& device, json& payload)
+{
+}
+
+void Plugin::deviceDidConnect(std::string& device, json& deviceInfo)
+{
+}
+
+void Plugin::deviceDidDisconnect(std::string& device)
+{
+}
+
+void Plugin::applicationDidLaunch(json& payload)
+{
+}
+
+void Plugin::applicationDidTerminate(json& payload)
+{
+}
+
+void Plugin::systemDidWakeUp()
+{
+}
+
+void Plugin::propertyInspectorDidAppear(std::string& action, std::string& context, std::string& device)
+{
+}
+
+void Plugin::sendToPlugin(std::string& action, std::string& context, json& payload)
+{
+}
+
+void Plugin::sendToPropertyInspector(std::string& action, std::string& context, json& payload)
+{
+}
+
+void Plugin::propertyInspectorDidDisappear(std::string& action, std::string& context, std::string& device)
+{
+}
---
+++ src/ESDCore/Plugin.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "JSONUtils.h"
+class Plugin
+{
+public:
+	Plugin();
+
+	virtual void didReceiveSettings(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void didReceiveGlobalSettings(json& payload);
+	virtual void keyDown(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void keyUp(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void willAppear(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void willDisappear(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void titleParametersDidChange(std::string& action, std::string& context, std::string& device, json& payload);
+	virtual void deviceDidConnect(std::string& device, json& deviceInfo);
+	virtual void deviceDidDisconnect(std::string& device);
+	virtual void applicationDidLaunch(json& payload);
+	virtual void applicationDidTerminate(json& payload);
+	virtual void systemDidWakeUp();
+	virtual void propertyInspectorDidAppear(std::string& action, std::string& context, std::string& device);
+	virtual void propertyInspectorDidDisappear(std::string& action, std::string& context, std::string& device);
+	virtual void sendToPlugin(std::string& action, std::string& context, json& payload);
+	virtual void sendToPropertyInspector(std::string& action, std::string& context, json& payload);
+};
+
