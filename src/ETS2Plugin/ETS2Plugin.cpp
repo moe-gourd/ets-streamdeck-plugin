41ea57c67140b218365e7a764725e2c87c22ddcf
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-23 21:16:21 +0200

    Glue parts together in main function

--- src/ETS2Plugin/ETS2Plugin.cpp
+++ src/ETS2Plugin/ETS2Plugin.cpp
@@ -2,8 +2,24 @@
 
 
 #include "ConnectionManager.h"
+#include "Connection.h"
+#include "CommandlineParser.h"
+#include "TestPlugin.h"
+
 
 int main(int argc, const char* const argv[])
 {
     ConnectionManager *connectionManager = new ConnectionManager();
+    Connection *connection = Connection::createWebsocketConnection(connectionManager);
+    connectionManager->setConnection(connection);
+
+    Plugin* plugin = new Plugin();
+    connectionManager->addPlugin(plugin);
+
+    CommandlineParser commandlineparser(argc, argv);
+    std::string registerEvent = commandlineparser.registerEvent();
+    std::string pluginUUID = commandlineparser.pluginUUID();
+    int port = commandlineparser.port();
+
+    connection->run(registerEvent, pluginUUID, port);
 }
