5e5db8c54a7267795589ae47d63c84ced66fdb69
Martin Kasper <github.10.marten@spamgourmet.com>
2021-04-25 12:56:40 +0200

    define action interface

--- src/ESDCore/Action.h
+++ src/ESDCore/Action.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include "JSONUtils.h"
+
 class Action
 {
+public:
+	virtual void didReceiveSettings(std::string& context, std::string& device, json& payload);
+	virtual void keyDown(std::string& context, std::string& device, json& payload);
+	virtual void keyUp(std::string& context, std::string& device, json& payload);
+	virtual void willAppear(std::string& context, std::string& device, json& payload);
+	virtual void willDisappear(std::string& context, std::string& device, json& payload);
+	virtual void titleParametersDidChange(std::string& context, std::string& device, json& payload);
+	virtual void propertyInspectorDidAppear(std::string& context, std::string& device);
+	virtual void propertyInspectorDidDisappear(std::string& context, std::string& device);
+	virtual void sendToPlugin(std::string& context, json& payload);
+	virtual void sendToPropertyInspector(std::string& context, json& payload);
 };
 
