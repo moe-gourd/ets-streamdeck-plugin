0353e2c3bb600bdfa2bc1750e94ef3dd7b116428
Martin Kasper <github.10.marten@spamgourmet.com>
2021-05-01 10:38:30 +0200

    add initial version of remaining SentEvents

--- src/ESDCore/EventsSent.cpp
+++ src/ESDCore/EventsSent.cpp
@@ -29,6 +29,58 @@ std::string SentEvent::dump()
 	return jsonMessage.dump();
 }
 
+SetSettingsEvent::SetSettingsEvent(std::string context) : SentEvent(std::string("setSettings"))
+{
+	this->context = context;
+}
+
+SetSettingsEvent::SetSettingsEvent(std::string context, json payload) : SentEvent(std::string("setSettings"))
+{
+	this->context = context;
+	setPayload(payload);
+}
+
+void SetSettingsEvent::setPayload(json payload)
+{
+	this->payload = payload;
+}
+
+GetSettingsEvent::GetSettingsEvent(std::string context) : SentEvent(std::string("getSettings"))
+{
+	this->context = context;
+}
+
+SetGlobalSettingsEvent::SetGlobalSettingsEvent(std::string context) : SentEvent(std::string("setGlobalSettings"))
+{
+	this->context = context;
+}
+
+SetGlobalSettingsEvent::SetGlobalSettingsEvent(std::string context, json payload) : SentEvent(std::string("setGlobalSettings"))
+{
+	this->context = context;
+	setPayload(payload);
+}
+
+void SetGlobalSettingsEvent::setPayload(json payload)
+{
+	this->payload = payload;
+}
+
+GetGlobalSettingsEvent::GetGlobalSettingsEvent(std::string context) : SentEvent(std::string("getGlobalSettings"))
+{
+	this->context = context;
+}
+
+OpenUrlEvent::OpenUrlEvent(std::string url) : SentEvent(std::string("openUrl"))
+{
+	payload["url"] = url;
+}
+
+LogMessageEvent::LogMessageEvent(std::string message) : SentEvent(std::string("logMessage"))
+{
+	payload["message"] = message;
+}
+
 SetTitleEvent::SetTitleEvent(std::string context) : SentEvent(std::string("setTitle"))
 {
 	this->context = context;
@@ -39,4 +91,88 @@ void SetTitleEvent::setTitle(std::string& text, int target, int state)
 	payload["title"] = text;
 	payload["target"] = target;
 	payload["state"] = state;
+}
+
+SetImageEvent::SetImageEvent(std::string context) : SentEvent(std::string("setImage"))
+{
+	this->context = context;
+}
+
+void SetImageEvent::setImage(std::string& image, int target, int state)
+{
+	payload["image"] = image;
+	payload["target"] = target;
+	payload["state"] = state;
+}
+
+ShowAlertEvent::ShowAlertEvent(std::string context) : SentEvent(std::string("showAlert"))
+{
+	this->context = context;
+}
+
+ShowOkEvent::ShowOkEvent(std::string context) : SentEvent(std::string("showOK"))
+{
+	this->context = context;
+}
+
+SetStateEvent::SetStateEvent(std::string context, int state) : SentEvent(std::string("setState"))
+{
+	this->setState(state);
+}
+
+void SetStateEvent::setState(int state)
+{
+	payload["state"] = state;
+}
+
+SwitchToProfileEvent::SwitchToProfileEvent(std::string context, std::string device) : SentEvent(std::string("switchToProfile"))
+{
+	this->device = device;
+}
+
+SwitchToProfileEvent::SwitchToProfileEvent(std::string context, std::string device, std::string profile) : SentEvent(std::string("switchToProfile"))
+{
+	this->device = device;
+	setProfile(profile);
+}
+
+void SwitchToProfileEvent::setProfile(std::string profile)
+{
+	payload["profile"] = profile;
+}
+
+SendToPropertyInspectorEventOut::SendToPropertyInspectorEventOut(std::string action, std::string context) :SentEvent(std::string("sendToPropertyInspector"))
+{
+	this->action = action;
+	this->context = context;
+}
+
+SendToPropertyInspectorEventOut::SendToPropertyInspectorEventOut(std::string action, std::string context, json payload) :SentEvent(std::string("sendToPropertyInspector"))
+{
+	this->action = action;
+	this->context = context;
+	setPayload(payload);
+}
+
+void SendToPropertyInspectorEventOut::setPayload(json payload)
+{
+	this->payload = payload;
+}
+
+SendToPluginEventOut::SendToPluginEventOut(std::string action, std::string context) : SentEvent(std::string("sendToPluginEvent"))
+{
+	this->action = action;
+	this->context = context;
+}
+
+SendToPluginEventOut::SendToPluginEventOut(std::string action, std::string context, json payload) : SentEvent(std::string("sendToPluginEvent"))
+{
+	this->action = action;
+	this->context = context;
+	setPayload(payload);
+}
+
+void SendToPluginEventOut::setPayload(json payload)
+{
+	this->payload = payload;
 }
--- src/ESDCore/EventsSent.h
+++ src/ESDCore/EventsSent.h
@@ -11,30 +11,46 @@ public:
 
 class SetSettingsEvent : public SentEvent
 {
+public:
+	SetSettingsEvent(std::string context);
+	SetSettingsEvent(std::string context, json payload);
+
+	void setPayload(json payload);
 
 };
 
 class GetSettingsEvent : public SentEvent
 {
+public:
+	GetSettingsEvent(std::string context);
 };
 
 class SetGlobalSettingsEvent : public SentEvent
 {
+public:
+	SetGlobalSettingsEvent(std::string context);
+	SetGlobalSettingsEvent(std::string context, json payload);
+
+	void setPayload(json payload);
 };
 
 class GetGlobalSettingsEvent : public SentEvent 
 {
-
+public:
+	GetGlobalSettingsEvent(std::string context);
 };
 
 class OpenUrlEvent : public SentEvent 
 {
+public:
+	OpenUrlEvent(std::string url);
 
 };
 
 class LogMessageEvent : public SentEvent
 {
-
+public:
+	LogMessageEvent(std::string message);
 };
 
 class SetTitleEvent : public SentEvent
@@ -47,36 +63,56 @@ public:
 
 class SetImageEvent : public SentEvent
 {
+public:
+	SetImageEvent(std::string context);
+	void setImage(std::string& image, int target = 0, int state = 0);
 
 };
 
 class ShowAlertEvent : public SentEvent
 {
-
+public:
+	ShowAlertEvent(std::string context);
 };
 
 class ShowOkEvent : public SentEvent
 {
+public:
+	ShowOkEvent(std::string context);
 
 };
 
 class SetStateEvent : public SentEvent
 {
+public:
+	SetStateEvent(std::string context, int state = 0);
+	void setState(int state);
 
 };
 
 class SwitchToProfileEvent : public SentEvent
 {
+public:
+	SwitchToProfileEvent(std::string context, std::string device);
+	SwitchToProfileEvent(std::string context, std::string device, std::string profile);
+	void setProfile(std::string profile);
 
 };
 
-class SendToPropertyInspectorSendEvent : public SentEvent
+class SendToPropertyInspectorEventOut : public SentEvent
 {
-	// FIXME send & receive
+public:
+	SendToPropertyInspectorEventOut(std::string action, std::string context);
+	SendToPropertyInspectorEventOut(std::string action, std::string context, json payload);
+	void setPayload(json payload);
+
 };
 
-class SendToPluginSendEvent : public SentEvent
+class SendToPluginEventOut : public SentEvent
 {
-	// FIXME send & receive
+public:
+	SendToPluginEventOut(std::string action, std::string context);
+	SendToPluginEventOut(std::string action, std::string context, json payloadd);
+	void setPayload(json payload);
 };
 
